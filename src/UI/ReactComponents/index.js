770d5a8efd31c5ede22afda428dd2190f49cf982
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-07-24 18:06:21 -0300

    creo ReactComponents dir

---
+++ src/UI/ReactComponents/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+function Escribiendo({ contenido, escribiendo, ingresar }) {
+  return (
+    <Container>
+      <p>Al terminar de escribir presione <i>Ingresar</i></p>
+      <TextField fullWidth autoFocus value={contenido} onChange={escribiendo} label="Producto" />
+      <br />
+      <br />
+      <Button onClick={() => ingresar(contenido)} color="primary" variant="contained">Ingresar</Button>
+    </Container>
+  )
+}
+function Esperando() {
+  return (
+    <CircularProgress />
+  )
+}
+function Limpio({ escribiendo }) {
+  return (
+    <Container>
+      <p>Ingrese productos al stock</p>
+      <TextField fullWidth autoFocus onChange={escribiendo} label="Producto" />
+      <br />
+      <br />
+      <Button disabled variant="contained">Ingresar</Button>
+    </Container>
+  )
+}
+function Error({ error, contenido, escribiendo }) {
+  return (
+    <Container>
+      <TextField fullWidth
+        autoFocus
+        error
+        helperText={error.message}
+        value={contenido}
+        onChange={escribiendo}
+        label="Producto" />
+      <br />
+      <br />
+      <Button disabled color="primary" variant="contained">Ingresar</Button>
+    </Container>
+  )
+}
+
+module.exports = {
+  Limpio,
+  Escribiendo,
+  Esperando,
+  Error
+}
--- src/UI/vista.js
+++ src/UI/vista.js
@@ -1,57 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import {
+  Limpio,
+  Escribiendo,
+  Esperando,
+  Error
+} from "./ReactComponents";
 
 const container = document.getElementById("input");
-
-function Escribiendo({ contenido, escribiendo, ingresar }) {
-  return (
-    <Container>
-      <p>Al terminar de escribir presione <i>Ingresar</i></p>
-      <TextField fullWidth autoFocus value={contenido} onChange={escribiendo} label="Producto" />
-      <br />
-      <br />
-      <Button onClick={() => ingresar(contenido)} color="primary" variant="contained">Ingresar</Button>
-    </Container>
-  )
-}
-function Esperando() {
-  return (
-    <CircularProgress />
-  )
-}
-function Limpio({ escribiendo }) {
-  return (
-    <Container>
-      <p>Ingrese productos al stock</p>
-      <TextField fullWidth autoFocus onChange={escribiendo} label="Producto" />
-      <br />
-      <br />
-      <Button disabled variant="contained">Ingresar</Button>
-    </Container>
-  )
-}
-function Error({ error, contenido, escribiendo }) {
-  return (
-    <Container>
-      <TextField fullWidth
-        autoFocus
-        error
-        helperText={error.message}
-        value={contenido}
-        onChange={escribiendo}
-        label="Producto" />
-      <br />
-      <br />
-      <Button disabled color="primary" variant="contained">Ingresar</Button>
-    </Container>
-  )
-}
 
 function mostrar(tree) {
   ReactDOM.render(
