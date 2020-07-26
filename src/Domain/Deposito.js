8bc31449ad83b906a53042f4ad0e211344c2694f
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-07-26 13:42:55 -0300

    meto firebase para persistir productos

--- src/Domain/Deposito.js
+++ src/Domain/Deposito.js
@@ -1,17 +1,26 @@
-const CLAVE = "deposito"
-
 class LocalStorageDeposito {
   async guardar(nombreProducto) {
-    const productosString = localStorage.getItem(CLAVE);
-    const productos = productosString ? JSON.parse(productosString) : [];
-    const productosActualizados = [...productos, nombreProducto];
-    localStorage.setItem(CLAVE, JSON.stringify(productosActualizados))
+    const productos = await this.obtenerTodos();
+    await put([...productos, nombreProducto]);
   }
-  obtenerTodos() {
-    const productosString = localStorage.getItem(CLAVE);
-    const productos = productosString ? JSON.parse(productosString) : [];
+  async obtenerTodos() {
+    const productos = await get();
     return productos;
   }
+}
+
+function put(productos) {
+  return fetch("https://inventarius-20ba9.firebaseio.com/productos.json", {
+    method: "PUT",
+    body: JSON.stringify(productos),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+}
+function get() {
+  return fetch("https://inventarius-20ba9.firebaseio.com/productos.json")
+    .then(res => res.json())
 }
 
 export default LocalStorageDeposito;
