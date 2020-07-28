f23cc46b97514092ee53a56713bbce4dfd7ee3b7
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-07-28 19:04:42 -0300

    cambio ul por tabla de material ui

--- src/UI/InterfazOrdenDeCompra.js
+++ src/UI/InterfazOrdenDeCompra.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const container = document.getElementById("root");
 
@@ -13,10 +20,28 @@ class InterfazOrdenDeCompra {
 }
 
 function OrdenDeCompra({ productos }) {
-  const children = productos.map((producto, i) => <li key={i}>{producto}</li>)
   return (
-    <ul>{children}</ul>
-  )
+    <section style={{ padding: "10px", paddingBottom: "50px" }}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Orden de compra ({productos.length})</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {productos.map(producto => (
+              <TableRow key={producto}>
+                <TableCell component="th" scope="row">
+                  {producto}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </section>
+  );
 }
 
 export default InterfazOrdenDeCompra;
