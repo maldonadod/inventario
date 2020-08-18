037a88ad75d79ccd260adbdc9c9dc37f76a14d51
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-08-17 21:34:23 -0300

    agrego acordion

--- src/UI/InterfazOrdenDeCompra.js
+++ src/UI/InterfazOrdenDeCompra.js
@@ -4,12 +4,13 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 
 const container = document.getElementById("root");
 
@@ -30,24 +31,26 @@ function OrdenDeCompra({ productos }) {
         direction="row"
         alignItems="center">
         <Container>
-          <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Orden de compra ({productos.length})</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {productos.map(producto => (
-                  <TableRow key={producto}>
-                    <TableCell component="th" scope="row">
-                      {producto}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <Accordion>
+            <AccordionSummary>
+              <span>Orden de compra ({productos.length})</span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <TableContainer>
+                <Table aria-label="simple table">
+                  <TableBody>
+                    {productos.map(producto => (
+                      <TableRow key={producto}>
+                        <TableCell component="td" scope="row">
+                          {producto}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </AccordionDetails>
+          </Accordion>
         </Container>
       </Grid>
     </Box>
